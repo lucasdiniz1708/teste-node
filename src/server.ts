8c2bc9501b49f3a675be54bf564888674694f2f1
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2024-06-05 18:44:18 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
-const server = express();
+const app = express();
+
+// Middleware para habilitar o CORS
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({ message: 'home' });
+});
+
+app.use((req: Request, res: Response) => {
+    res.status(404).send({ message: 'página não encontrada' });
+});
+
 const PORT = process.env.PORT || 4000;
-
-server.get('/', (req, res) => {
-    res.send('home');
-});
-
-server.use((req, res) => {
-    res.send('página não encontrada');
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
-}); 
+});
